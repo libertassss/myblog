53ba4f74de849167166884460b9087a1e1247b0c
wjw0215 <821060818@qq.com>
2018-11-28 17:24:30 +0800

    service层

---
+++ src/main/java/com/zyy/blog/modules/sys/service/UserService.java
@@ -0,0 +1,12 @@
+package com.zyy.blog.modules.sys.service;
+
+import com.zyy.blog.modules.sys.entity.User;
+
+public interface UserService {
+
+    /**
+     * 数据插入
+     */
+    int insert(User user);
+
+}
