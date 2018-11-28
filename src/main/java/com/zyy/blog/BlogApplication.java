ccdb958e77390908db0a3a9787dedd138b9ea338
libertas1 <2767389215@qq.com>
2018-11-28 18:08:37 +0800

    hh

--- src/main/java/com/zyy/blog/BlogApplication.java
+++ src/main/java/com/zyy/blog/BlogApplication.java
@@ -1,11 +1,14 @@
 package com.zyy.blog;
 
 
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import static org.springframework.boot.SpringApplication.*;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.zyy.blog.modules.sys.dao"})
 public class BlogApplication {
 
 	public static void main(String[] args) {
