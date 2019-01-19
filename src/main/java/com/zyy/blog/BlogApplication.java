48894a2fcdc836656c28a2ffc0854473f3f858c0
wjw0215 <821060818@qq.com>
2019-01-19 12:50:49 +0800

    pom修改

--- src/main/java/com/zyy/blog/BlogApplication.java
+++ src/main/java/com/zyy/blog/BlogApplication.java
@@ -3,12 +3,12 @@ package com.zyy.blog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.zyy.blog.modules.sys.dao"})
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
